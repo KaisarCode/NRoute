59cfa1bfe58e6dadf8b047c741203301c752b2a8
KaisarCode <kaisar@kaisarcode.com>
2020-07-11 01:12:08 -0300

    rollback

--- demo.js
+++ demo.js
@@ -1,18 +1,8 @@
 var app = require('./index');
 
 app.use(function(req, res, next){
-    next();
-    console.log(req.url);
-});
-
-app.get(/\/test\/(\d+)/ig, function(req, res, next){
-    var id = req.pmatch[0];
-    var out = 'URL ID: '+id;
-    res.end(out);
-});
-
-app.use(function(req, res, next){
-    res.end(''); 
+   var out = req.url;
+   res.end(out);
 });
 
 // HTTP
